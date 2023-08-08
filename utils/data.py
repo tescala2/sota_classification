957f520458a50cb025bd5b0d5bfb03180581db02
tescala2 <trex.escalante@gmail.com>
2023-08-08 13:26:59 -0400

    corrected loops and dataloader creation

--- utils/data.py
+++ utils/data.py
@@ -6,11 +6,10 @@ from torchvision.io import read_image
 
 
 class ImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+    def __init__(self, annotations_file, img_dir, transform=None):
         self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
         self.transform = transform
-        self.target_transform = target_transform
 
     def __len__(self):
         return len(self.img_labels)
@@ -21,18 +20,19 @@ class ImageDataset(Dataset):
         label = self.img_labels.iloc[idx, 1]
         if self.transform:
             image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
         return image, label
 
 
-def get_loaders(path, bs, shuffle, transform=None, target_transform=None):
-    loaders = []
-    for mode in ['train', 'val', 'test']:
-        annotations_path = f'{path}/{mode}/annotations.csv'
-        img_path = f'{path}/{mode}/images'
+def get_dataloader(path, mode, bs, transform, shuffle=True, num_workers=24):
+    dataset = ImageDataset(
+        annotations_file=f'{path}/data/{mode}/chips/labels.csv',
+        img_dir=f'{path}/data/{mode}/chips/images/',
+        transform=transform
+    )
+    dataloader = DataLoader(dataset,
+                            batch_size=bs,
+                            shuffle=shuffle,
+                            num_workers=num_workers,
+                            pin_memory=True)
 
-        data = ImageDataset(annotations_path, img_path, transform=transform, target_transform=target_transform)
-        loaders.append(DataLoader(data, batch_size=bs, shuffle=shuffle))
-
-    return loaders
+    return dataloader
--- utils/loops.py
+++ utils/loops.py
@@ -1,18 +1,30 @@
+import os
+import time
 import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
+import pandas as ps
 
-from sota_classification.utils.data import get_loaders
+from torch import save, load
+from .data import get_dataloader
 
 
-def train(dataloader, model, criterion, optimizer, device):
+def train(dataloader, model, criterion, optimizer, device, progress=False, parallel=False):
+    if parallel:
+        assert torch.cuda.device_count() > 1, "Attempted to use multiple GPUs, but only 1 GPU is available"
+        model = nn.DataParallel(model, device_ids=[0, 1, 2])
+    model.to(device)
     model.train()
     num_batches = len(dataloader)
 
     total_loss = 0
+    total_correct = 0
+    total_images = 0
+    start = time.time()
     for i, (X, y) in enumerate(dataloader):
 
-        X, y = X.to(device), y.to(device)
+        X, y = X.float().to(device), y.to(device)
 
         optimizer.zero_grad()
 
@@ -23,59 +35,135 @@ def train(dataloader, model, criterion, optimizer, device):
         optimizer.step()
 
         total_loss += loss.item()
+        scores, classes = output.max(dim=1)
+        total_correct += (classes == y).sum().item()
+        total_images += len(X)
+        end = time.time()
+        if progress:
+            print(i + 1, '/', num_batches, f'batches trained in {(end - start) / 60:.2f} minutes')
 
     avg_loss = total_loss / num_batches
+    avg_acc = total_correct / total_images
 
-    return avg_loss
+    return avg_loss, avg_acc
 
 
-def evaluate(dataloader, model, criterion, device):
+def evaluate(dataloader, model, criterion, device, progress=False, parallel=False):
+    if parallel:
+        assert torch.cuda.device_count() > 1, "Attempted to use multiple GPUs, but only 1 GPU is available"
+        model = nn.DataParallel(model, device_ids=[0, 1, 2])
+    model.to(device)
     model.eval()
     num_batches = len(dataloader)
 
-    preds = []
-    labels = []
     total_loss = 0
+    total_correct = 0
+    total_images = 0
     with torch.no_grad():
+        start = time.time()
         for i, (X, y) in enumerate(dataloader):
-            X, y = X.to(device), y.to(device)
+            X, y = X.float().to(device), y.to(device)
 
             output = model(X)
             total_loss += criterion(output, y).item()
-
-            preds.extend([pred.item() for pred in output])
-            labels.extend([label.item() for label in y])
+            scores, classes = output.max(dim=1)
+            total_correct += (classes == y).sum().item()
+            total_images += len(X)
+            end = time.time()
+            if progress:
+                print(i + 1, '/', num_batches, f'batches evaluated in {(end - start) / 60:.2f} minutes')
 
     avg_loss = total_loss / num_batches
+    avg_acc = total_correct / total_images
 
     return avg_loss
 
 
-def run(path, model, name, device, transform=None, epochs=10, lr=0.001, bs=16):
+def run(data_path,
+        model,
+        name,
+        device,
+        epochs=10,
+        lr=0.001,
+        bs=16,
+        progress=False,
+        parallel=False,
+        cont=False):
+    if cont:
+        assert os.path.exists(f'best-{name}-losses.csv'), 'No weights to continue from'
+        weights_path = f'best-{name}-weights.pth'
+        weights = load(weights_path)
+        model.load_state_dict(weights, strict=True)
+
+    if os.path.exists(f'best-{name}-losses.csv'):
+        best_val_loss = pd.read_csv(f'best-{name}-losses.csv').iloc[-1]['val loss']
+    else:
+        best_val_loss = np.inf
+
+    print(f'Initial Validation Loss: {best_val_loss:.5f}')
+
+    val_loader = get_dataloader(data_path, 'val', bs=bs)
+    test_loader = get_dataloader(data_path, 'test', bs=bs)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                           factor=0.5,
+                                                           patience=1,
+                                                           threshold=0.001,
+                                                           verbose=True,
+                                                           min_lr=1e-5,
+                                                           threshold_mode='abs')
 
-    best_val_loss = np.inf  # evaluate(val_loader, model, criterion, device, debug=True, mode='Val')
-    # print(f"Initial Validation Loss: {best_val_loss:.5f}")
-
+    start = time.time()
+    metrics = []
     for epoch in range(epochs):
-        train_loader, val_loader, test_loader = get_loaders(path=path,
-                                                           bs=bs,
-                                                           shuffle=True,
-                                                           transform=transform)
+        train_loader = get_dataloader(data_path, 'train', bs=bs)
 
-        train_loss = train(train_loader, model, criterion, optimizer, device)
-        val_loss = evaluate(val_loader, model, criterion, device)
+        train_loss, train_acc = train(train_loader,
+                                      model,
+                                      criterion,
+                                      optimizer,
+                                      device,
+                                      progress=progress,
+                                      parallel=parallel)
+        val_loss, val_acc = evaluate(val_loader,
+                                     model,
+                                     criterion,
+                                     device,
+                                     progress=progress,
+                                     parallel=parallel)
 
-        info = f"Epoch {epoch+1}: Training Loss: {train_loss:.5f}\tValidation Loss: {val_loss:.5f}"
+        scheduler.step(val_loss)
 
+        metrics.append([train_loss, train_acc, val_loss, val_acc])
+        save(model.state_dict(), f'latest-{name}-weights.pth')
+
+        info = f"Epoch {epoch + 1}: Training Loss: {train_loss:.5f}\t Training Accuracy: {train_acc * 100:.2f}%" \
+               f"Validation Loss: {val_loss:.5f}\tValidation Accuracy: {val_acc * 100:.2f}%"
+
+        best = False
         if best_val_loss > val_loss:
-            torch.save(model.state_dict(), name + '.pth')
+            best = True
+            torch.save(model.state_dict(), f'best-{name}-weights.pth')
             best_val_loss = val_loss
 
-            info += "\tCheckpoint!"
+            test_loss, test_acc = evaluate(test_loader,
+                                           model,
+                                           criterion,
+                                           device,
+                                           progress=False,
+                                           parallel=parallel)
 
-            test_loss = evaluate(test_loader, model, criterion, device)
+            info += f"\tCheckpoint!\n\t\t Test Loss: {test_loss:.5f}\tTest Accuracy: {test_acc * 100:.2f}%"
 
-        print(info)
+        end = time.time()
+        print(info + f'\nTime elapsed: {(end - start) / 60:.2f} minutes')
+        if best:
+            pd.DataFrame(
+                metrics,
+                columns=['train loss', 'train acc', 'val loss', 'val acc']
+            ).to_csv(
+                f'best-{name}-losses.csv',
+                index=False
+            )
